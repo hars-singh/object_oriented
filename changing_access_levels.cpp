2685074e861429876cec14b023a86c289872fd1f
Harsh Singh <53997601+Analystharsh@users.noreply.github.com>
2019-09-22 03:53:52 -0700

    Create changing_access_levels.cpp

---
+++ changing_access_levels.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class student{
+protected:
+string name;
+public:
+int age;
+void setage(int iage){age =iage;}
+};
+
+class puppy:private student{
+  public:
+  student :: name;
+  student :: age;
+int id;
+void setid(int iid){id =iid;}
+void display(){
+
+  cout<<"My name is "<<name<<"and my age is "<<age<<". I do have a shop which was named as"<<" by my grand father "<<"and one more thing that regestration number of my shop is "<<id<<"."<<endl;
+}
+void ssetage(int iage){
+  setage(iage);
+  }
+
+};
+
+int main()
+{
+puppy student1;
+student1.name="Harsh Singh";/*Now as we have changed the access level of name varibale from private to public by using resolution operator so we can change it's value.*/
+student1.ssetage(20);
+student1.setid(234678);
+student1.display();
+}
